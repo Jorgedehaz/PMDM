0942dcdd476257f6ce3f59e96de838117cdebee0
Dehaz <jorgedehaz147@gmail.com>
2024-10-24 09:30:31 +0200

    Prueba movils

--- BotonesEventos/app/src/main/java/com/example/botoneseventos/MainActivity.java
+++ BotonesEventos/app/src/main/java/com/example/botoneseventos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.botoneseventos;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,15 +21,42 @@ public class MainActivity extends AppCompatActivity {
         respuesta=findViewById(R.id.respuesta);
     }
 
-    public void onClickBtnSi(View view) {
-        respuesta.setText("Has pulsado SI");
+    public void onClickBtn(View view) {
+        switch(view.getId()) {
+            case (R.id.btnsi):
+                respuesta.setText("Has pulsado SI");
+                break;
+            case (R.id.btnno):
+                respuesta.setText("Has pulsado NO");
+                break;
+            case (R.id.btnaveces):
+                respuesta.setText("Has pulsado A VECES");
+                break;
+        }
     }
 
-    public void onClickBtnNo(View view) {
-        respuesta.setText("Has pulsado NO");
-    }
 
-    public void onClickBtnAveces(View view) {
-        respuesta.setText("Has pulsado A VECES");
-    }
+
+
+//    public void onClickBtn(View view) {
+//        if (view.getId() == R.id.btnsi){
+//            respuesta.setText("Has pulsado SI");
+//        }
+//        else if (view.getId() == R.id.btnno){
+//            respuesta.setText("Has pulsado NO");
+//        }else
+//            respuesta.setText("Has pulsado A VECES");
+//    }
+
+//    public void onClickBtnSi(View view) {
+//        respuesta.setText("Has pulsado SI");
+//    }
+//
+//    public void onClickBtnNo(View view) {
+//        respuesta.setText("Has pulsado NO");
+//    }
+//
+//    public void onClickBtnAveces(View view) {
+//        respuesta.setText("Has pulsado A VECES");
+//    }
 }
